86c6be42e71c43c0c9b6fd8a91611fb1197aae29
Judson Santana <76632803+sunrocha123@users.noreply.github.com>
2021-02-10 17:16:20 -0300

    Reformulação do programa
    Remover duplicidade de código.

--- menu_Jogo_NIM.py
+++ menu_Jogo_NIM.py
@@ -1,73 +1,25 @@
 import partida
 
-class tipos_de_partida:
-
-    def __init__(self, numero_de_pecas, limite_pecas_por_jogada):
-        self.numero_de_pecas = numero_de_pecas
-        self.limite_pecas_por_jogada = limite_pecas_por_jogada
-
-    def caminho(self):
-        return partida.Partidas()
-
-    def partida_unica(self):
-        print('Você escolheu uma partida isolada!\n')
-        print('******PARTIDA******\n')
-
-        partida_isolada = self.validar_quem_comeca()
-
-        if partida_isolada == 1:
-            print('Fim do jogo! Você ganhou!\n')
-        else:
-            print('Fim do jogo! O computador ganhou!\n')
-        pass
-
-    def campeonato(self):
-        print('Você escolheu um campeonato!\n')
-        vencedor_usuario = 0
-        vencedor_maquina = 0
-
-        for i in range(3):
-            print(str(i + 1) + 'º Disputa\n')
-
-            partida_campeonato = self.validar_quem_comeca()
-
-            if partida_campeonato == 1:
-                vencedor_usuario += 1
-                print('Você ganhou a ' + str(i + 1) + 'º disputa\n')
-            else:
-                vencedor_maquina += 1
-                print('O computador ganhou a ' + str(i + 1) + 'º disputa\n')
-        print('Placar: Você', vencedor_usuario, 'X', vencedor_maquina, 'Computador')
-
-    def validar_quem_comeca(self):
-        if self.numero_pecas % (self.limite_pecas_por_jogada + 1) == 0:
-            return self.caminho().usuario_comeca(self.numero_pecas, self.limite_pecas_por_jogada)
-        else:
-            return self.caminho().computador_comeca(self.numero_pecas, self.limite_pecas_por_jogada)
 
 class cabecalho_e_coleta_dados:
 
     def cabecalho(self):
         print('Bem-vindo ao jogo do NIM!\n')
-        dados_necessarios = self.coletar_dados()
-        tipos_de_partida(dados_necessarios[0], dados_necessarios[1])
         print('\nOpções de jogo:\n')
         print('1- Para jogar uma partida isolada\n2- Para jogar um campeonato\n')
         escolha = int(input('Digite uma opção para iniciarmos o jogo: '))
         caminho = tipos_de_partida()
         
         if escolha == 1:
+            print('Você escolheu partida única!\n')
             caminho.partida_unica()
         else:
+            print('Você escolheu campeonato!\n')
             caminho.campeonato()
-
-    def coletar_dados(self):
-        numero_pecas = int(input('Digite a quantidade de peças no tabuleiro:  '))
-        limite_pecas_por_jogada = int(input('Digite o limite de retirada de peça por jogada: '))
-        return numero_pecas, limite_pecas_por_jogada
 
 def iniciar_programa():
     caminho = cabecalho_e_coleta_dados()
     caminho.cabecalho()
 
 iniciar_programa()
+
--- partida.py
+++ partida.py
@@ -1,3 +1,100 @@
+class coleta_dados:
+
+    def coletar_dados(self):
+        numero_pecas = int(input('Digite a quantidade de peças que terá no tabuleiro: '))
+        limite_de_retirada_de_pecas = int(input('Digite o limite de peças que podem ser retiradas do tabuleiro: '))
+
+        if numero_pecas % (limite_de_retirada_de_pecas + 1) == 0:
+            quem_comeca = 1
+        else:
+            quem_comeca = 0
+
+        return numero_pecas, limite_de_retirada_de_pecas, quem_comeca
+
+
+class Partida:
+
+    def jogar(self, quantidade_de_partidas):
+
+        caminho_coleta_dados = coleta_dados()
+
+        for i in range(quantidade_de_partidas):
+            dados_necessarios = caminho_coleta_dados.coletar_dados()
+            numero_pecas_tabuleiro = dados_necessarios[0]
+            quem_vence = dados_necessarios[2]
+                
+    def usuario_escolhe_jogada(self, limite_pecas_por_jogada):
+        '''while True:
+            quant_pecas = int(input('Quantas peças você vai retirar? '))
+            if (quant_pecas > 0 and quant_pecas <= limite_pecas_por_jogada):
+                return quant_pecas
+                pass
+            else:
+                print('Oops! Jogada inválida! Tente de novo.')
+                pass
+            pass''' # Campo desabilitado para realização de testes
+        return limite_pecas_por_jogada - 1
+
+    def computador_escolhe_jogada(self, numero_pecas, limite_pecas_por_jogada):
+        if numero_pecas <= limite_pecas_por_jogada:
+            return numero_pecas
+            pass
+        else:
+            if numero_pecas % (limite_pecas_por_jogada + 1) > 0:
+                return numero_pecas % (limite_pecas_por_jogada + 1)
+                pass
+            return limite_pecas_por_jogada
+            pass            
+
+
+
+
+
+'''class tipos_de_partida:
+
+    def __init__(self, numero_de_pecas, limite_pecas_por_jogada):
+        self.numero_de_pecas = numero_de_pecas
+        self.limite_pecas_por_jogada = limite_pecas_por_jogada
+
+    def caminho(self):
+        return partida.Partidas()
+
+    def partida_unica(self):
+        print('Você escolheu uma partida isolada!\n')
+        print('******PARTIDA******\n')
+
+        partida_isolada = self.validar_quem_comeca()
+
+        if partida_isolada == 1:
+            print('Fim do jogo! Você ganhou!\n')
+        else:
+            print('Fim do jogo! O computador ganhou!\n')
+        pass
+
+    def campeonato(self):
+        print('Você escolheu um campeonato!\n')
+        vencedor_usuario = 0
+        vencedor_maquina = 0
+
+        for i in range(3):
+            print(str(i + 1) + 'º Disputa\n')
+
+            partida_campeonato = self.validar_quem_comeca()
+
+            if partida_campeonato == 1:
+                vencedor_usuario += 1
+                print('Você ganhou a ' + str(i + 1) + 'º disputa\n')
+            else:
+                vencedor_maquina += 1
+                print('O computador ganhou a ' + str(i + 1) + 'º disputa\n')
+        print('Placar: Você', vencedor_usuario, 'X', vencedor_maquina, 'Computador')
+
+    def validar_quem_comeca(self):
+        if self.numero_pecas % (self.limite_pecas_por_jogada + 1) == 0:
+            return self.caminho().usuario_comeca(self.numero_pecas, self.limite_pecas_por_jogada)
+        else:
+            return self.caminho().computador_comeca(self.numero_pecas, self.limite_pecas_por_jogada)'''
+
 class Partidas:
 
     def usuario_comeca(self, numero_pecas, limite_pecas_por_jogada):
@@ -36,27 +133,4 @@ class Partidas:
             if numUsuario == 0:
                 return 1
             print('Agora restam', numero_pecas, 'peças no tabuleiro.')
-            pass
-
-    def usuario_escolhe_jogada(self, limite_pecas_por_jogada):
-        '''while True:
-            quant_pecas = int(input('Quantas peças você vai retirar? '))
-            if (quant_pecas > 0 and quant_pecas <= limite_pecas_por_jogada):
-                return quant_pecas
-                pass
-            else:
-                print('Oops! Jogada inválida! Tente de novo.')
-                pass
-            pass''' # Campo desabilitado para realização de testes
-        return limite_pecas_por_jogada - 1
-
-    def computador_escolhe_jogada(self, numero_pecas, limite_pecas_por_jogada):
-        if numero_pecas <= limite_pecas_por_jogada:
-            return numero_pecas
-            pass
-        else:
-            if numero_pecas % (limite_pecas_por_jogada + 1) > 0:
-                return numero_pecas % (limite_pecas_por_jogada + 1)
-                pass
-            return limite_pecas_por_jogada
             pass
