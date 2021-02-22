43bed2d9bedea27d19ed5ac51b1f85adc4906981
Judson Santana <76632803+sunrocha123@users.noreply.github.com>
2021-02-21 21:23:43 -0300

    Dando sequência na criação da interface

--- menu_Jogo_NIM.py
+++ menu_Jogo_NIM.py
@@ -24,42 +24,55 @@ class cabecalho_e_coleta_dados:
             janela_opcao = sg.Window('Opções de partida', informacoes_opcao)
 
             event, values = janela_opcao.read()
+            janela_opcao.close()
             if event == 'Campeonato':
                 self.campeonato()
-            elif event == sg.WINDOW_CLOSED:
-                janela_opcao.close()
             else:
                 self.partida_unica()
+
+    def caminho(self):
+        return partida.Partida()
     
     def partida_unica(self):
 
-            '''caminho = partida.Partida()
-            
-            if escolha == 1:
-                print('\nVocê escolheu partida única!\n')
-                vencedor = caminho.jogar()
+        vencedor = self.caminho().jogar()
 
-                if vencedor == 1:
-                    print('Fim do jogo! Você ganhou!\n')
+        if vencedor == 1:
+            quem_venceu = [[sg.Text('Fim do jogo! Você ganhou!')]]
+        else:
+            quem_venceu = [[sg.Text('Fim do jogo! O computador ganhou!')]]
+
+        janela_vencedor = sg.Window('Vencedor', quem_venceu)
+        
+        while True:
+            event, values = janela_vencedor.read()
+            if event == sg.WINDOW_CLOSED:
+                break
+
+    '''def campeonato(self):
+            pontuacao_usuario = 0
+            pontuacao_maquina = 0
+
+            pontuacao = [[sg.Text('Usuário:', str(pontuacao_usuario))],
+                         [sg.Text('Máquina:', str(pontuacao_maquina))]]
+
+            janela_pontuacao = sg.Window('Placar', pontuacao)
+
+            while True:
+                event, values = janela_pontuacao.read()
+                if event == sg.WINDOW_CLOSED:
+                    break
                 else:
-                    print('Fim do jogo! O computador ganhou!\n')'''
-
-    def campeonato(self):
-
-            '''else:
-                print('\nVocê escolheu campeonato!')
-                pontuacao_usuario = 0
-                pontuacao_maquina = 0
-                for i in range(3):
-                    print(str(i + 1) + 'º Disputa\n')
-                    vencedor = caminho.jogar()
-                    if vencedor == 1:
-                        pontuacao_usuario += 1
-                        print('Você ganhou a ' + str(i + 1) + 'º disputa\n')
-                    else:
-                        pontuacao_maquina += 1
-                        print('O computador ganhou a ' + str(i + 1) + 'º disputa\n')
-                print('Placar: Você', pontuacao_usuario, 'X', pontuacao_maquina, 'Computador')'''
+                    for i in range(3):
+                        print(str(i + 1) + 'º Disputa\n')
+                        vencedor = self.caminho().jogar()
+                        if vencedor == 1:
+                            pontuacao_usuario += 1
+                            print('Você ganhou a ' + str(i + 1) + 'º disputa\n')
+                        else:
+                            pontuacao_maquina += 1
+                            print('O computador ganhou a ' + str(i + 1) + 'º disputa\n')
+                    print('Placar: Você', pontuacao_usuario, 'X', pontuacao_maquina, 'Computador')'''
 
 def iniciar_programa():
     caminho = cabecalho_e_coleta_dados()
